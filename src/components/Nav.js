fe406e64073a8b1ce6bd0c0fd1bf563730aa9167
Citlalli27 <70440578+Citlalli27@users.noreply.github.com>
2022-07-17 14:14:07 -0500

    Cambio letra

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,8 @@ function Nav() {
   return (
     <div className='BarraNavegacion'>
         <ul className='ListaBarra'>
-            <li> <a> INICIO </a></li>
-            <li> <a> PERFIL </a></li>
+            <li> <a > INICIO </a></li>
+            <li> <a > PERFIL </a></li>
             <li> <a> NOTICIAS </a></li>
             <li> <a> CRYPTO </a></li>
         </ul>        

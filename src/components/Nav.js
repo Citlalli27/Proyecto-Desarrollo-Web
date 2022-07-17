17ad91db2e368e611df6262e363fb49dcb39a212
Citlalli27 <70440578+Citlalli27@users.noreply.github.com>
2022-07-17 14:08:50 -0500

    Primeros cambios

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Nav.css';
+function Nav() {
+  return (
+    <div className='BarraNavegacion'>
+        <ul className='ListaBarra'>
+            <li> <a> INICIO </a></li>
+            <li> <a> PERFIL </a></li>
+            <li> <a> NOTICIAS </a></li>
+            <li> <a> CRYPTO </a></li>
+        </ul>        
+    </div>
+  )
+}
+
+export default Nav

8eedce98150d7fb5f38939c00c1442e21ae4334a
MarioAderman <mario.aderman@gmail.com>
2022-07-17 17:00:01 -0500

    El estilo cambió un poco y navegación agregada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 import Nav from "./components/Nav";
+import Inicio from "./components/Inicio";
+import Perfil from "./components/Perfil";
+import Noticias from "./components/Noticias";
+import Crypto from "./components/Crypto";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="Body">
-    <Nav/> 
-  
-    </div>
+    <BrowserRouter>
+    <Nav />
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/noticias" element={<Noticias />} /> 
+        <Route path="/crypto" element={<Crypto />} />   
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Nav.css';
+
 function Nav() {
   return (
     <div className='BarraNavegacion'>
         <ul className='ListaBarra'>
-            <li> <a > INICIO </a></li>
-            <li> <a > PERFIL </a></li>
-            <li> <a> NOTICIAS </a></li>
-            <li> <a> CRYPTO </a></li>
+            <li> <a href="/" > INICIO </a></li>
+            <li> <a href="/perfil" > PERFIL </a></li>
+            <li> <a href="/noticias" > NOTICIAS </a></li>
+            <li> <a href="/crypto" > CRYPTO </a></li>
         </ul>        
     </div>
   )

c0ced6d4e7033576c659d62af817a11dfe02b8c7
MarioAderman <mario.aderman@gmail.com>
2022-07-17 22:43:16 -0500

    Inicio casi listo, falta arreglar contenedores

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Inicio.css';
 
 function Inicio() {
   return (
-    <div>Inicio</div>
+    <div className='container'>
+      <aside className='aside'>
+      <div className='userProfile'>UserName </div>
+      <div className='Recent'>Summary</div>
+      </aside>
+      <ul className='Placeholder'>
+        <li className='Post'>
+        Lorem Impsum
+        </li>
+        <li className='Post'>
+        Lorem Impsum
+        </li>
+        <li className='Post'>
+        Lorem Impsum
+        </li>
+      </ul>
+    </div>
   )
 }
 

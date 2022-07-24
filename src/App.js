1a818d12c6a37db97a4f311dfc15791af5a48696
MarioAderman <mario.aderman@gmail.com>
2022-07-24 12:56:43 -0500

    Nuevo diseño Sign up

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Perfil from "./components/Perfil";
 import Noticias from "./components/Noticias";
 import Crypto from "./components/Crypto";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sign from "./components/Sign";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
         <Route path="/" element={<Inicio />} />
         <Route path="/perfil" element={<Perfil />} />
         <Route path="/noticias" element={<Noticias />} /> 
-        <Route path="/crypto" element={<Crypto />} />   
+        <Route path="/crypto" element={<Crypto />} />
+        <Route path="/sign" element={<Sign />} />   
       </Routes>
     </BrowserRouter>
     
